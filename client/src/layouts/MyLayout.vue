f052c9bc10fb64e4f4359675281afc89f921e474
IskraNikolova <air_iskra@yahoo.com>
2020-01-14 20:42:41 +0200

    init app, added web3 js lib

---
+++ client/src/layouts/MyLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Home
+        </q-toolbar-title>
+
+        <div>right</div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
